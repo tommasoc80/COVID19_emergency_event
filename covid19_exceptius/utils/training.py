1062f2deeac0a68a854dfda8b09a2ac505d631bf
gtziafas <pullmypill@hotmail.com>
2021-03-30 08:31:44 +0300

    refactoring

--- covid19_exceptius/utils/training.py
+++ covid19_exceptius/utils/training.py
@@ -1,72 +1,56 @@
+from covid19_exceptius.types import *
+from covid19_exceptius.utils.metrics import get_metrics
+
 import torch
-from torch import Tensor, LongTensor
-from torch.nn import Module 
-from torch.utils.data import DataLoader
-from torch.optim import Optimizer
-
-from typing import Callable, Tuple, Dict
-
-tensor_map = Callable[[Tensor], Tensor]
 
 
-@torch.no_grad()
-def multi_label_metrics(preds: Tensor, labels: LongTensor, thresh: float=0.5) -> Dict[str, float]:
-    batch_size, num_classes = preds.shape
-
-    # for evaluation, make all logit predictions >50% to become 1 and other 0
-    preds = torch.where(preds.sigmoid()>thresh, torch.ones_like(preds), torch.zeros_like(preds))
-
-    # accuracy score counts the percentage of correctly classified entire sentences
-    corrects = preds == labels 
-    count_mask = corrects.sum(dim=-1)
-    accuracy = count_mask[count_mask==num_classes].shape[0] / batch_size
-
-    # hamming loss counts the fraction of incorrectly classified specific labels
-    hamm_loss = corrects[corrects==0].shape[0] / (batch_size * num_classes)
-
-    return {'accuracy':accuracy, 'hamming_loss':hamm_loss}
-
-
-def train_epoch(model: Module, dl: DataLoader, optim: Optimizer, loss_fn: tensor_map, device: str) -> Tuple[float, float, float]:
+def train_epoch(model: Module, dl: DataLoader, optim: Optimizer, loss_fn: Module, device: str) -> Dict[str, Any]:
     model.train()
 
-    batch_loss, batch_accu, batch_hamm_loss = 0., 0., 0.
-    for batch_idx, (x,y,t) in enumerate(dl):
+    epoch_loss = 0.
+    all_preds:  List[List[int]] = []
+    all_labels: List[List[int]] = []
+
+    for batch_idx, (x, y) in enumerate(dl):
         x = x.to(device)
         y = y.to(device)
-        t = t.to(device)
         
         # forward 
-        predictions = model(x, t)
+        predictions = model.forward(x)
         loss = loss_fn(predictions, y.float())
-        acc_metrics = multi_label_metrics(predictions, y)
 
         # backprop
         loss.backward()
         optim.step()
         optim.zero_grad()
 
-        batch_loss += loss.item()
-        batch_accu += acc_metrics['accuracy']
-        batch_hamm_loss += acc_metrics['hamming_loss']
+        all_preds.extend(predictions.sigmoid().round().cpu().tolist())
+        all_labels.extend(y.cpu().tolist())
 
-    return batch_loss/len(dl), batch_accu/len(dl), batch_hamm_loss/len(dl) 
+        epoch_loss += loss.item()
+
+    return {'BCEloss': round(epoch_loss/len(dl), 5), **get_metrics(all_preds, all_labels)}
 
 
 @torch.no_grad()
-def eval_epoch(model: Module, dl: DataLoader, loss_fn: tensor_map, device: str) -> Tuple[float, float, float]:
+def eval_epoch(model: Module, dl: DataLoader, loss_fn: Module, device: str) -> Dict[str, Any]:
     model.eval()
 
-    batch_loss, batch_accu, batch_hamm_loss = 0., 0., 0.
-    for batch_idx, (x,y,t) in enumerate(dl):
+    epoch_loss = 0.
+    all_preds: List[List[int]] = []
+    all_labels: List[List[int]] = []
+
+    for batch_idx, (x, y) in enumerate(dl):
         x = x.to(device)
         y = y.to(device)
-        t = t.to(device)
-        predictions = model(x, t)
-        loss = loss_fn(predictions, y.float())
-        acc_metrics = multi_label_metrics(predictions, y)
-        batch_loss += loss.item()
-        batch_accu += acc_metrics['accuracy']
-        batch_hamm_loss += acc_metrics['hamming_loss']
 
-    return batch_loss/len(dl), batch_accu/len(dl), batch_hamm_loss/len(dl) 
+        # forward
+        predictions = model.forward(x)
+        loss = loss_fn(predictions, y.float())
+
+        all_preds.extend(predictions.sigmoid().round().cpu().tolist())
+        all_labels.extend(y.cpu().tolist())
+
+        epoch_loss += loss.item()
+
+    return {'BCEloss': round(epoch_loss/len(dl), 5), **get_metrics(all_preds, all_labels)}
