a7f3297cd2340ca8d827b8c394b2424ecff782fb
gtziafas <pullmypill@hotmail.com>
2021-04-15 09:06:36 +0300

    metrics only for samples with events

--- covid19_exceptius/utils/metrics.py
+++ covid19_exceptius/utils/metrics.py
@@ -7,15 +7,21 @@ def _round(x: float) -> float:
 
 
 def get_metrics(preds: List[List[int]], labels: List[List[int]]) -> Dict[str, Any]:
-    per_column_preds = list(zip(*preds))
-    per_column_labels = list(zip(*labels))
+    event_idces = [i for i, l in enumerate(labels) if l != [False] * len(l)]
+    labels_with_events = [l for i, l in enumerate(labels) if i in event_idces]
+    preds_with_events = [p for i, p in enumerate(preds) if i in event_idces]
+
+    per_column_preds = list(zip(*preds_with_events))
+    per_column_labels = list(zip(*labels_with_events))
     per_column_metrics = [(f1_score(pcl, pcp, labels=[1, 0], average='weighted'),
                            recall_score(pcl, pcp, labels=[1, 0], average='weighted'),
                            precision_score(pcl, pcp, labels=[1, 0], average='weighted'))
                           for pcp, pcl in zip(per_column_preds, per_column_labels)]
     f1s, ps, rs = list(zip(*per_column_metrics))
-    return {'sentence_accuracy': _round(accuracy_score(array(labels), array(preds))),
-            'event_accuracy': _round(1 - hamming_loss(array(labels), array(preds))),
+    return {'total_accuracy': _round(accuracy_score(array(labels), array(preds))),
+            'hamming_accuracy': _round(1 - hamming_loss(array(labels), array(preds))),
+            'event_accuracy_sent': _round(accuracy_score(array(labels_with_events), array(preds_with_events))),
+            'event_accuracy_label': _round(1 - hamming_loss(array(labels_with_events), array(preds_with_events))),
             'mean_f1': _round(sum(f1s)/len(f1s)),
             'mean_precision': _round(sum(ps)/len(ps)),
             'mean_recall': _round(sum(rs)/len(rs)),
